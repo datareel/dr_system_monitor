115a5880c157f8967a9c94081430d48af6b06bd7
Douglas.Gaer@noaa.gov <Douglas.Gaer@noaa.gov>
2017-05-04 15:51:26 -0500

    added alert test script

--- bin/purge.sh
+++ bin/purge.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # ----------------------------------------------------------- 
 # BASH Script
 # Operating System(s): RHEL/CENTOS
---
+++ bin/test_alert.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "${BASEdir}" == "" ]; then export BASEdir="${HOME}/drsm"; fi
+
+if [ -f ${HOME}/.drsm.sh ]; then
+    echo "INFO - Using config override file ${HOME}/.drsm.sh"
+    CONFIG_FILE=${HOME}/.drsm.sh
+else
+    echo "INFO - Using default config ${BASEdir}/etc/drsm.sh"
+    CONFIG_FILE=${BASEdir}/etc/drsm.sh
+fi
+
+if [ ! -f ${CONFIG_FILE} ]; then
+    echo "Missing base config ${CONFIG_FILE}"
+    exit 1
+fi
+
+source ${CONFIG_FILE}
+
+TIMESPAN="1"
+
+SUBJECT="[!ACTION!] Alert Test Message"
+BODY="This is an alert test message"
+
+source ${DRSMHOME}/bin/text_email_alert.sh
+email_alert "${SUBJECT}" "${BODY}"
+
+exit 0
