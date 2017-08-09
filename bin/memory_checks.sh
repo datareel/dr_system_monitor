1be03c1a8b7ed787705a92c7d92ed316b5cec3f4
dgaerhome <dgaerhome@gmail.com>
2017-08-09 10:26:41 -0400

    Updates for memory checking script

--- bin/memory_checks.sh
+++ bin/memory_checks.sh
@@ -6,9 +6,9 @@
 # Shell: BASH shell
 # Original Author(s): DataReel Software Development
 # File Creation Date: 05/25/2013
-# Date Last Modified: 05/03/2017
+# Date Last Modified: 08/09/2017
 #
-# Version control: 1.12
+# Version control: 1.13
 #
 # Contributor(s):
 # ----------------------------------------------------------- 
@@ -52,11 +52,17 @@ total_swap=$(free -mt | grep Swap | awk '{print $2}')
 used_swap=$(free -mt | grep Swap | awk '{print $3}')
 free_swap=$(free -mt | grep Swap | awk '{print $4}')
 free=$(free -mt | grep Total | awk '{print $4}')
+buffers_kb=$(cat /proc/meminfo | grep -E '^Buffers:' | awk '{ print $2 }')
+cache_kb=$(cat /proc/meminfo | grep -E '^Cached:' | awk '{ print $2 }')
+buffers=$(echo "${buffers_kb} / 1024" | bc)
+cache=$(echo "${cache_kb} / 1024" | bc)
+MemAvailable_kb=$(cat /proc/meminfo | grep -E '^MemAvailable:' | awk '{ print $2 }')
+MemAvailable=$(echo "${buffers} + ${cache} + ${free_physical}" | bc)
 
 # Get our top processes
 PROCS=$(ps -eo %mem,pid,user,args | grep -v '%MEM' | grep -v " 0.0 " | sort -rn | head -n $NUMPROCS)
 
-if [ $free -le $ALERT ] 
+if [ $MemAvailable -le $ALERT ] 
 then
     echo "ERROR - ${HOST} total free memory is below ${free} MB"
     has_error="1"
@@ -64,14 +70,19 @@ fi
 
 if [ $used_swap -ge $SWAPALERT ] 
 then
-    echo "WARNING - ${HOST} is using ${used_swap} MB of SWAP space"
-    has_warning="1"
+    if [ $used_swap -gt $MemAvailable ]; then
+	echo "WARNING - ${HOST} is using ${used_swap} MB of SWAP space"
+	has_warning="1"
+    fi
 fi
 
 echo "Memory stats for ${HOST}"
 echo "Total physical: ${total_physical} MB"
 echo "Used physical:  ${used_physical} MB"
 echo "Free physical:  ${free_physical} MB"
+echo "Buffers:  ${buffers} MB"
+echo "Cache:  ${cache} MB"
+echo "Physical Memory Available: ${MemAvailable} MB"
 echo "Total SWAP: ${total_swap} MB"
 echo "Used SWAP:  ${used_swap} MB"
 echo "Free SWAP:  ${free_swap} MB"
